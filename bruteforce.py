65eb537d8db84f909d40e643431592d080e7f15b
cedrik8200 <cedrik8200@cedrik8200-Nitro-AN515-42>
2022-02-03 08:37:36 +0000

    script bruteforce fini et fonctionnel

--- bruteforce.py
+++ bruteforce.py
@@ -1,10 +1,7 @@
 import csv
-from itertools import permutations
-import cProfile
-import re
 
 
-# initialise le budget maximun client et le chemin du fichier d'action
+# initialise le budget maximun client et le chemin de fichier d'action
 CLIENT_BUDGET = 500.00
 ROOT_CSV = "fichier_d_action/action.csv"
 
@@ -19,127 +16,84 @@ def recovery_action_list():
             name = row["name"]
             cout = float(row["price"])
             benefice = float(row["profit"])
+            valeur = cout / 100 * benefice
             list_action[name] = {
                 "price": cout,
                 "profit": benefice,
+                "profit_valeur": valeur,
             }
-        print("\n fichier csv télécharger")
-        return list_action
+    return list_action
+
+
+def tri_list(data):
+    """tri les liste sur la profit en valeur en ordre decroissant"""
+
+    tableau_trier = sorted(data.items(), key=lambda k:k[1]["profit_valeur"], reverse = True)
+    return tableau_trier
 
 
 def sort_list_by_profit(tab):
-    """creer toutes les combinaisons possible et return une liste"""
+    """algorithme de style glouton itère sur la liste tab et ajoute dans le panier les actions
+        selectionner, s'arrête quand le montant du panier à atteint la valeur du budget client"""
 
-    comb = {}
-    compteur = 0
-    nombre = len(tab)
-    print("\ncréation des combinaisons possibles")
-    print("merci de patienter ...\n")
+    panier = CLIENT_BUDGET
+    benefice_panier = 0
+    cout_panier = 0
+    liste_action = []
 
-    for key in permutations(tab.keys(), nombre):
-        compteur += 1
-        list = "liste_" + str(compteur)
-        action = {}
-        for i in key:
-            name = i
-            action[i] = tab.get(name)
-        comb[list] = action
-
-    return comb
-
-def data_brute(data_brute):
-    """transforme DATABRUTE en liste et retour une liste de panier"""
-
-    liste_panier = {}
-    compteur = 0
-    for data in data_brute.values():
-        compteur += 1
-        panier = "panier"+ str(compteur)
-        liste = create_bascket_list(data)
-        liste_panier[panier] = liste
-
-    return liste_panier
-
-
-def create_bascket_list(data):
-    """creation de panier d'action d'un montant défini par le CLIENT_BUDGET initialiser
-    en haut du script et return un tableau"""
-
-    cout_global = 0.00
-    panier = []
-    compteur = 0
-
-    for i, j in data.items():
-        compteur += 1
-        cout_action = j.get("price")
-        ecart = CLIENT_BUDGET - cout_global
-
-        if cout_action <= ecart:
-            cout_global += cout_action
-            choix = {i: j}
-            panier.append(choix)
-
-    return panier
-
-
-def statistique_panier(tab):
-    """calcul le cout et le profit global du panier d'action rentré en parametre et return
-    ces informations sous forme de liste"""
-
-    panier = {}
-    paniers = []
-
-    for key, value in tab.items():
-        cout_panier = 0.00
-        profit_panier = 0.00
-        for i in value:
-            for k, v in i.items():
-                cout = v.get("price")
-                profit = cout * v.get("profit") / 100
-                cout_panier += cout
-                profit_panier += profit
-        panier[key] = {
-                "profit_panier": profit_panier,
-                "cout_panier": cout_panier,
-                "liste_actions": value,
-            }
-        paniers.append(panier)
-
-    return paniers
-
-
-def choice_best_list(datas):
-    """tri à bulles calibré sur le pourcentage et return un tableau"""
-
-    data = datas[1]
-
-    list_actions = sorted(data.items(), key=lambda k: k[1]["profit_panier"], reverse=True)
-
-    return list_actions
+    while panier > 0:
+        for i in tab:
+            v = i[1]
+            cout_action = v.get("price")
+            valeur = v.get("profit_valeur")
+            if cout_action > panier:
+                pass
+            else:
+                panier -= cout_action
+                liste_action.append(i)
+                benefice_panier += valeur
+                cout_panier += cout_action
+    return liste_action
 
 
 def add_customer_basket():
     """appel des fonctions necessaire pour la creation d'un panier d'action avec un budget
-    pre_défini, calibré sur le meilleur profit et affiche en console le resultat"""
+    pre_défini, calibré sur le meilleur profit"""
 
-    list_action = recovery_action_list()
-    paniers = statistique_panier(data_brute(sort_list_by_profit(list_action)))
+    data = recovery_action_list()
+    tab = tri_list(data)
 
-    liste = choice_best_list(paniers)
+    panier = sort_list_by_profit(tab)
 
-    panier_trier = liste[0]
-    basket = panier_trier[1]
+    print("\nrecherche de la meilleur combinaison")
+    print("merci de patienter ...\n")
 
-    valeur_profit = panier_trier[1].get("profit_panier")
-    cout_d_action = basket.get("cout_panier")
-    liste_d_action = basket.get("liste_actions")
-
-    print("\n\nliste d'action des meilleurs actions :\n")
-    for i in liste_d_action:
-        print(i)
-    print("\ncout de ce panier :\n", cout_d_action, "€\n")
-    print("cout de ce panier :\n", valeur_profit, "€\n\n")
+    print_statistique_panier(panier)
 
 
-# appel defonction
-cProfile.run(add_customer_basket())
+def print_statistique_panier(data):
+    """affichage du détails du panier d'actions choisit par l'algorithme"""
+
+    valeur_profit = 0
+    cout_action = 0
+
+    print("\nliste d' actions pour le meilleurs randement :\n")
+
+    for i in data:
+        v = i[1]
+        profit = v.get("profit_valeur")
+        cout = v.get("price")
+        valeur_profit += profit
+        cout_action += cout
+        print(i[0])
+
+    print("\ncout de ce panier :\n", cout_action, "€\n")
+    print("profit de ce panier :\n", valeur_profit, "€\n\n")
+
+
+# appel de fonction
+add_customer_basket()
+
+
+
+
--- optimized.py
+++ optimized.py
@@ -1,146 +1,99 @@
 import csv
 
 
-# initialise le budget maximun client et les chemins des fichiers d'actions
+# initialise le budget maximun client et le chemin de fichier d'action
 CLIENT_BUDGET = 500.00
-ROOT_CSV = "fichier_d_action/dataset2_Python+P7.csv"
+ROOT_CSV = "fichier_d_action/dataset1_Python+P7.csv"
 
 
 def recovery_action_list():
     """lecture d'un fichier csv et le transforme en liste"""
 
-    list_action = []
+    list_action = {}
     with open(ROOT_CSV, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             name = row["name"]
             cout = float(row["price"])
             benefice = float(row["profit"])
-            t = [name, cout, benefice]
-            list_action.append(t)
-        return list_action
+            valeur = cout / 100 * benefice
+            list_action[name] = {
+                "price": cout,
+                "profit": benefice,
+                "profit_valeur": valeur,
+            }
+    return list_action
 
 
-def first_tri_list(data):
-    """enleve les actions de valeur négative ou égole à 0 et return un tableau"""
+def tri_list(data):
+    """tri les liste sur la profit en valeur en ordre decroissant"""
 
-    tab = []
-    for i in data:
-        cout = i[1]
-        if cout > 0:
-            tab.append(i)
-
-    return tab
+    tableau_trier = sorted(data.items(), key=lambda k:k[1]["profit_valeur"], reverse = True)
+    return tableau_trier
 
 
-def sort_list_by_profit_percentage(tab):
-    """tri à bulles calibré sur le pourcentage et return un tableau"""
+def sort_list_by_profit(tab):
+    """algorithme de style glouton itère sur la liste tab et ajoute dans le panier les actions
+        selectionner, s'arrête quand le montant du panier à atteint la valeur du budget client"""
 
-    n = len(tab)
-    for i in tab:
-        i.reverse()
+    panier = CLIENT_BUDGET
+    benefice_panier = 0
+    cout_panier = 0
+    liste_action = []
 
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if tab[j+1] < tab[j]:
-                tab[j+1], tab[j] = tab[j], tab[j+1]
-
-    for i in tab:
-        i.reverse()
-
-    tab.reverse()
-    return tab
-
-
-def sort_list_by_profit_value(tab):
-    """tri a bulles calibré sur le pourcentage en valeur et return un tableau"""
-
-    n = len(tab)
-    for i in tab:
-        cout = i[1]
-        pourcentage = i[2]
-        valeur = cout*pourcentage/100
-        i.append(valeur)
-
-    for i in tab:
-        i.reverse()
-
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            print("ligne 71:", j)
-            if tab[j + 1] < tab[j]:
-                print("ligne 73:", tab[j])
-                tab[j + 1], tab[j] = tab[j], tab[j + 1]
-
-    for i in tab:
-        i.reverse()
-
-    tab.reverse()
-    return tab
-
-
-def create_bascket_list(data):
-    """creation de panier d'action d'un montant défini par le CLIENT_BUDGET initialiser
-    en haut du script et return un tableau"""
-
-    cout_global = 0.00
-    panier = []
-
-    for i in data:
-        cout_action = i[1]
-        ecart = CLIENT_BUDGET - cout_global
-        if cout_action <= ecart:
-            cout_global += cout_action
-            panier.append(i)
-
-    return panier
-
-
-def statistique_panier(tab):
-    """calcul le cout et le profit global du panier d'action rentré en parametre et return
-    ces informations sous forme de liste"""
-
-    cout_panier = 0.00
-    profit_panier = 0.00
-
-    for i in tab:
-        cout = i[1]
-        profit = cout * i[2] / 100
-        cout_panier += cout
-        profit_panier += profit
-
-    panier = [cout_panier, profit_panier]
-    return panier, tab
-
-
-def choice_best_list(list1, list2):
-    """choisit le panier le plus rentable entre ces deux panier et return ce choix sous
-    forme d'une liste contement un tableau et une liste"""
-
-    panier1 = statistique_panier(create_bascket_list(list1))
-    panier2 = statistique_panier(create_bascket_list(list2))
-
-    if panier1[1] > panier2[1]:
-        return panier1
-    else:
-        return panier2
+    while panier > 0:
+        for i in tab:
+            v = i[1]
+            cout_action = v.get("price")
+            valeur = v.get("profit_valeur")
+            if cout_action > panier:
+                pass
+            else:
+                panier -= cout_action
+                liste_action.append(i)
+                benefice_panier += valeur
+                cout_panier += cout_action
+    return liste_action
 
 
 def add_customer_basket():
     """appel des fonctions necessaire pour la creation d'un panier d'action avec un budget
-    pre_défini calibré sur le meilleur profit et affiche en console le resultat"""
+    pre_défini, calibré sur le meilleur profit"""
 
-    list_action = first_tri_list(recovery_action_list())
-    list_action2 = first_tri_list(recovery_action_list())
-    retour = choice_best_list(sort_list_by_profit_value(list_action), sort_list_by_profit_percentage(list_action2))
+    data = recovery_action_list()
+    tab = tri_list(data)
 
-    valeur_profit = retour[0]
-    panier_d_action = retour[1]
+    print("\ngénération des combinaisons")
+    print("merci de patienter ...\n")
+    print()
 
-    print("\nliste d'action dans le panier du client :\n")
-    for i in panier_d_action:
-        print(i)
-    print("\ncout et profit de ce panier :\n", valeur_profit, "\n")
+    panier = sort_list_by_profit(tab)
+
+    print("\nvaleurisation des combinaisons")
+    print("merci de patienter ...\n")
+    print()
+
+    print_statistique_panier(panier)
+
+
+def print_statistique_panier(data):
+    """affichage du détails du panier d'actions choisit par l'algorithme"""
+
+    valeur_profit = 0
+    cout_action = 0
+
+    print("\n\nliste d' actions pour le meilleurs randement :\n")
+
+    for i in data:
+        v = i[1]
+        profit = v.get("profit_valeur")
+        cout = v.get("price")
+        valeur_profit += profit
+        cout_action += cout
+        print(i[0])
+
+    print("\ncout de ce panier :\n", cout_action, "€\n")
+    print("profit de ce panier :\n", valeur_profit, "€\n\n")
 
 
 # appel de fonction
